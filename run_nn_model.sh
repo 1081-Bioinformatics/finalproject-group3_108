ebef8ea0d5b95b4db3a7f234157e8fea3fe610aa
Jiayu_Lu <jeanie0807@gmail.com>
2020-01-07 08:26:32 +0800

    Update docs.

--- run_nn_model.sh
+++
@@ -1,6 +0,0 @@
-python ./code/model/nn/nn_model.py \
-    --x_train ./data/control/control_x_train.npy \
-    --y_train ./data/control/control_y_train.npy \
-    --x_test ./data/control/control_x_test.npy  \
-    --y_test ./data/control/control_y_test.npy
-
